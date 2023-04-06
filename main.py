9b2ffee10a7f1c607cf481a86d879b0a900005b9
IU14 <iu14@canterbury.ac.uk>
2023-04-06 11:10:52 +0100

    installing packages but still not working

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ import pandas as pd #imports data processing
 #from sklearn.cluster import KMeans
 
 #open files & extract the data 
-file1 = open("/workspace/KMeansClusteringAssignment/DataSets/adult.data", "r")
+file1 = open("C:\Users\i_gil\OneDrive - Canterbury Christ Church University\Documents\Year 2\S2 - Artifical Intelligence Computing\Assignment 2\code\DataSets\adult.data", "r")
 
-for line in file1
+for line in file1:
     datalines = file1.readline()
     list1 = datalines.split(',')
-    features.append([float(list1[0].strip()),float(list1[1].strip())]) #
+    list1.append([float(list1[0].strip()),float(list1[1].strip())]) #
 file1.close()
 
-file2 = open("/workspace/KMeansClusteringAssignment/DataSets/adult.test", "r")
+file2 = open("C:\Users\i_gil\OneDrive - Canterbury Christ Church University\Documents\Year 2\S2 - Artifical Intelligence Computing\Assignment 2\code\DataSets\adult.testt", "r")
 
-for line in file2
+for line in file2:
     datalines2 =  file2.readline()
     list2 = datalines2.split (',')
-    featuers.append([float(list2[0].strip()),float(list2[1].strip())]) #
+    list2.append([float(list2[0].strip()),float(list2[1].strip())]) #
 file2.close()
 
 #add both extracted data lists together
@@ -27,6 +27,6 @@ print(allData)
 
 
 #set number of clusters
-k =
+#k =
 
 
