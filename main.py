d5e8c58ea7a41aa2f22860638f57b8f378015d48
Issy <iu14@canterbury.ac.uk>
2023-04-17 09:22:40 +0000

    Applied Standard scaler and produced an elbow graph to find K

--- main.py
+++ main.py
@@ -6,22 +6,24 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
 #opens the data, sets it as a table using the 3rd column as the index as this is the unique piece of data
-
 datafile = pd.read_csv('adult.csv', index_col = [2])
 
 # drops any null data from the data set
 datafile.dropna(inplace = True)
 
+#transform data using standard scaler to plot easier
+scaler = StandardScaler()
+datafile.columns = datafile.columns.to_series().apply(lambda x: x.strip())
+datafile[["age_t", "educationNum_t", "capitolGain_t", "capitolLoss_t", "hours_t"]] = scaler.fit_transform(datafile[["age", "educationNum", "capitolGain", "capitolLoss", "hours"]])
 #print(datafile)
 
 #function to find optimum number of clusters, k
-
 def findKnumber(data, max_k):
     means = []
-    interias = []
+    inertias = []
     
     for k in range (1, max_k):
-        kmeans = KMeans(n_clsuters=k)
+        kmeans = KMeans(n_clusters=k)
         kmeans.fit(data)
         
         means.append(k)
@@ -35,4 +37,8 @@ def findKnumber(data, max_k):
     plt.show()
     
 #call function to find k
-findKnumber(datafile[['age', 'educationNum' ]], 10)
+findKnumber(datafile[["age_t", "educationNum_t", "capitolGain_t", "capitolLoss_t", "hours_t"]], 10)
+
+#applying Kmeans algorithm
+#elbow graph suggest 5 or 6 clusters 
+kmeans = KMeans 
