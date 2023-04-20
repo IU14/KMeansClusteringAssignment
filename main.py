a03e4949d2a626e468123cb6329c41535649c5ea
Issy <iu14@canterbury.ac.uk>
2023-04-20 11:26:28 +0000

    Amedned code to just change K to 6 and 4 rather than run through the loop

--- main.py
+++ main.py
@@ -53,16 +53,39 @@ plt.ylim(0, 20)
 plt.show()
 
 #plot results with varrying clusters
-for k in range(1, 6):
-    kmeans=KMeans(n_clusters=k)
-    kmeans.fit(datafile[["age_t", "educationNum_t"]])
-    datafile[f'KMeans_{k}'] = kmeans.labels_
-    print(datafile)
-    datafile.describe()
-    fig, axs = plt.subplots(nrows =1, ncols =5, figsize =(20,5))
-    print(len(axs))
-    for i, ax in enumerate(fig.axes, start=2):
-        ax.scatter(x=datafile['age'], y=datafile['educationNum'], c=datafile[f'KMeans_{i}'])
-        ax.set_xlim(15, 100)
-        ax.set_ylim(0, 20)
-        ax.set_title(f'N Clusters: {i}') 
+#for k in range(1, 6):
+    #kmeans=KMeans(n_clusters=k)
+    #kmeans.fit(datafile[["age_t", "educationNum_t"]])
+    #datafile[f'KMeans_{k}'] = kmeans.labels_
+    #datafile.describe()
+    #fig, axs = plt.subplots(nrows =1, ncols =5, figsize =(20,5))
+    #print(len(axs))
+    #for i, ax in enumerate(fig.axes, start=2):
+        #ax.scatter(x=datafile['age'], y=datafile['educationNum'], c=datafile[f'KMeans_{i}'])
+       # ax.set_xlim(15, 100)
+        #ax.set_ylim(0, 20)
+        #ax.set_title(f'N Clusters: {i}') 
+    
+#changing the K number
+kmeans = KMeans(n_clusters=6)
+kmeans.fit(datafile[["age_t", "educationNum_t"]])
+datafile['kmeans_5'] = kmeans.labels_
+#print(datafile)
+
+#plot the results using age and eductaion num as the axis. 
+plt.scatter(x=datafile['age'], y=datafile['educationNum'], c=datafile['kmeans_5'])
+plt.xlim(15, 100)
+plt.ylim(0, 20)
+plt.show()
+
+#changing the K number
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(datafile[["age_t", "educationNum_t"]])
+datafile['kmeans_5'] = kmeans.labels_
+#print(datafile)
+
+#plot the results using age and eductaion num as the axis. 
+plt.scatter(x=datafile['age'], y=datafile['educationNum'], c=datafile['kmeans_5'])
+plt.xlim(15, 100)
+plt.ylim(0, 20)
+plt.show()
